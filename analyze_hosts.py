897a1cb19583cc964513561e4ddf74a03bd5102a
Justin Moore <Kaemin@gmail.com>
2024-03-24 21:52:52 -0500

    Update analyze_hosts.py

--- analyze_hosts.py
+++ analyze_hosts.py
@@ -3,11 +3,7 @@
 
 """analyze_hosts - scans one or more hosts for security misconfigurations
 
-Copyright (C) 2015-2023 Peter Mosmans [Go Forward]
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+
 """
 
 import argparse
